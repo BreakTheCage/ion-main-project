49099e35f654663f18519d23ee14ab9927c0b7f3
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-07-15 00:20:09 +0400

    #111; Start Adding sideDrawer

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlacesService } from '../../places.service';
+import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
 
 @Component({
   selector: 'app-edit-offer',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
-
-  constructor() { }
+  place: Place;
+  constructor(
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      console.log('ParamMap: ', paramMap);
+      if(!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      console.log('Starting extract place with placeId of: ', paramMap.get('placeId'));
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      console.log('Selected Place: ', this.place);
+
+    });
   }
 
 }
